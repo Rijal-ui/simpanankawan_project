1edb1e93ff771a8d8c51c9c6be07dd4a149513d1
Rijal-ui <79946385+Rijal-ui@users.noreply.github.com>
2023-09-21 08:20:42 +0700

    Add files via upload
    First Commit

---
+++ app/src/main/java/com/kominfo/simpanankawan/ui/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.kominfo.simpanankawan.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kominfo.simpanankawan.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/com/kominfo/simpanankawan/ui/signup/SignupActivity.kt
@@ -0,0 +1,12 @@
+package com.kominfo.simpanankawan.ui.signup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kominfo.simpanankawan.R
+
+class SignupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+    }
+}
